669f963d2de7d12e34f7d4b6556a98c862ca366b
Barbara <barbara@barbaras-macbook-pro.local>
2018-02-10 14:10:27 -0500

    Initial Commit

---
+++ ClearyWordGarden/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClearyWordGarden
+//
+//  Created by Barbara on 2/10/18.
+//  Copyright © 2018 Barbara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
